f06fad91eca6e08fc73707c192e9e22008d38389
bigneji <bigneji@gmail.com>
2025-05-26 08:06:55 -0700

    done

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -14,7 +14,7 @@ db.restaurant = require('../models/index') (sequelize , DataTypes)
 
 sequelize.authenticate()
 .then(()=>{
-     console.log("✅ Connected to database successfully")
+     console.log("✅ Connected to database successfullyyyyyyyyyyyy")
 })
 .catch((error)=>{
     console.log("❌ Unable to connect to the database:" , error)
